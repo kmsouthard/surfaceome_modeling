e3abb567410f72cfd856c673429ff041332c3d8a
kmsouthard <kade.southard@gmail.com>
2020-12-30 15:36:54 -0500

    added init file

---
+++ code/tidy_code/surfaceomeTopography/_init_.py
@@ -0,0 +1,7 @@
+from database_parsing import *
+from download_proteome import *
+from identify_domains import *
+from memory_optimization import *
+from parse_mobidb_annotations import *
+from phyre2_parsing import *
+from plotting_size import *
