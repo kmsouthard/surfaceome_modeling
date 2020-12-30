d2e41bb6845abd3aa3dbd921588c70772a908e28
kmsouthard <kade.southard@gmail.com>
2020-12-30 15:43:47 -0500

    changes to download_proteome

--- code/tidy_code/surfaceomeTopography/_init_.py
+++ code/tidy_code/surfaceomeTopography/_init_.py
@@ -1,5 +1,5 @@
 from database_parsing import *
-from download_proteome import *
+from download_proteome import get_proteome
 from identify_domains import *
 from memory_optimization import *
 from parse_mobidb_annotations import *
--- code/tidy_code/surfaceomeTopography/download_proteome.py
+++ code/tidy_code/surfaceomeTopography/download_proteome.py
@@ -10,15 +10,15 @@ First you need to go to http://www.uniprot.org/proteomes/ and download lists of 
         - 'tsv' HAS to be present, separated from the identifier by a single period
 The actual trick of the URL to use was found here: https://www.biostars.org/p/292993/ (top answer as of 2018-01-29)
 
+__version__ = '0.1.0'
+__author__ = 'Jamie Heather'
+
 script downloaded 8/13/19 and modified by Kaden Southard
 modified for use with python 3.7.3
 
 example output_file_type: "fasta", "tab", "gff"
 
 """
-
-__version__ = '0.1.0'
-__author__ = 'Jamie Heather'
 
 import gzip
 import os
@@ -27,9 +27,9 @@ import io
 
 #if __name__ == '__main__':
 
-def get_proteome(path,output_file_type, output_path):    
-    
-    # Get files
+def get_proteome(path,output_file_type, output_path):
+
+    # Get tab files containing proteomes of interest
     all_files = [x for x in os.listdir(path) if 'tab' in x]
     all_files.sort()
 
@@ -61,42 +61,42 @@ def get_proteome(path,output_file_type, output_path):
         # Open file, read in accessions
         with opener(path+'/'+f, mode = 'r') as in_file:
             line_count = 0
-            
+
             #generate list of accessions
             if opener == open:
-                
+
                 for line in in_file:
-                    
+
                     #first line is header
                     if line_count == 0:
-                        headers = line.rstrip().split('\t')    
+                        headers = line.rstrip().split('\t')
                     else:
                         bits = line.rstrip().split('\t')
                         accessions.append(bits[0])
-                    
+
                     line_count += 1
-            
+
             #compensation for a gzip.open not reading as str
             else:
-                
+
                 for line in in_file:
-                
+
                     #first line is header
                     if line_count == 0:
-                        headers = line.decode('utf8').rstrip().split('\t')    
+                        headers = line.decode('utf8').rstrip().split('\t')
                     else:
                         bits = line.decode('utf8').rstrip().split('\t')
                         accessions.append(bits[0])
-                        
+
                     line_count += 1
-                    
+
         for accession in accessions:
-            
+
             #create individual file name for proteome
             out_name = base_name + accession + '.'+output_file_type
-            
+
             print("retrieving "+accession+" data...")
-            
+
             # Determine full URL, pull the data and write to output file
             if output_file_type == 'tab':
                 #need to specify columns for tab format download
@@ -104,13 +104,12 @@ def get_proteome(path,output_file_type, output_path):
                 f= open(output_path+'/'+out_name, 'w')
                 f.write(url.text)
                 f.close()
-                
+
             else:
                 url = requests.get(url_prefix + accession + url_suffix)
                 f= open(output_path+'/'+out_name, 'w')
                 f.write(url.text)
                 f.close()
-            
+
             #for url_line in url:
              #   gzip.open(path+'/'+out_name, 'w').write(url_line)
-                
