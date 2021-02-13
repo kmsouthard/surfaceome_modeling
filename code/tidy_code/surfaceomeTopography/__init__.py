af21eda12f886a757de7a462797bb5110c5eaba1
kmsouthard <kade.southard@gmail.com>
2021-02-13 14:07:17 -0500

    add disorder annotations to model

--- code/tidy_code/surfaceomeTopography/__init__.py
+++ code/tidy_code/surfaceomeTopography/__init__.py
@@ -2,6 +2,6 @@ from .download_proteome import get_proteome
 from .database_parsing import *
 from .identify_domains import *
 from .memory_optimization import *
-#from .parse_mobidb_annotations import *
+from .mobidb_annotations import *
 from .phyre2_parsing import *
 from .plotting_size import *
