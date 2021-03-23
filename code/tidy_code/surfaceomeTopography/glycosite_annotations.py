ed168576a49301316078f9463c8b3f919e4b72d5
kmsouthard <kade.southard@gmail.com>
2021-03-23 13:41:20 -0400

    parse_glycoMine

--- code/tidy_code/surfaceomeTopography/glycosite_annotations.py
+++ code/tidy_code/surfaceomeTopography/glycosite_annotations.py
@@ -18,3 +18,17 @@ def surface_glycosites(glycosylation, surfaceome):
                     &(surfaceome_gly['glycosite'] >= surfaceome_gly['start'])]
 
     return ecd_glycosites
+
+def parse_glycoMine(glycoMine, surfaceome_accessions):
+
+    glycoMine['ID link'] = glycoMine['UniProtID']
+
+    glycosylation = glycoMine[glycoMine['ID link'].isin(surfaceome_accessions)]
+
+    glycosylation = glycosylation.assign(glycosite = glycosylation['Site'])
+    glycosylation.drop(columns = ['UniProtID', 'Site'], inplace = True)
+
+    return glycosylation
+
+def disorder_glycosylation():
+    return None
